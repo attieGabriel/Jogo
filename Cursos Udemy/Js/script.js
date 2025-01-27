4034ee6a8ed12f5b2b7cc755022c62e98a97f3a6
Gabriel Gomes Attie <gabrielgattie@gmail.com>
2025-01-27 15:15:54 -0300

    Colocar minhas coisas no github

---
+++ Cursos Udemy/Js/script.js
@@ -0,0 +1,15 @@
+var mensagem = "Hello, World!";
+var a =2;
+var b = 3;
+
+var soma = a + b;
+var sub = a - b;
+var mult = a * b;
+var div = a / b;
+var resto = a % b;
+
+if(b%2 == 1){
+    alert("B é impar");
+}else{
+    alert("B é par");
+}
